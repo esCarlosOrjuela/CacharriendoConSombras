8f147849e70aba200da023a7a3bad7873342325b
Carlos Orjuela <ingkrlosorjuela@evertecinc.com>
2022-12-15 17:26:32 -0500

    Ejercicio practico
    - Agregamos el paquete Sharpnado.Shadows a los tres proyectos (Compartido, iOS y Android)

---
+++ Sombras/Sombras/PageSombras.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Sombras
+{
+    public partial class PageSombras : ContentPage
+    {
+        public PageSombras()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
